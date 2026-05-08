7dc71d9dec19b33add154340edcd3ad8dfdf2bbe
valbniym <hecer1436@gmail.com>
2026-05-08 23:09:53 +0300

    Update input.h

--- qms-source/kernel/func/input.h
+++ qms-source/kernel/func/input.h
@@ -1,7 +1,3 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
- * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
-// kernel/func/input.h
 #ifndef _FUNC_INPUT_H
 #define _FUNC_INPUT_H
 
